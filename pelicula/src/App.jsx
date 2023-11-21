f5468bb6e1c344666d426edc49a990f534c56dd2
Nicolas Tambosco <tambosconicolas123@gmail.com>
2023-11-21 10:41:16 -0300

    .

--- pelicula/src/App.jsx
+++ pelicula/src/App.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-useEffect(() => {
-  // Llamada a la API para obtener datos de películas
-  fetch('URL_DE_LA_API')
-    .then((response) => response.json())
-    .then((data) => setMovieData(data))
-    .catch((error) => console.error('Error fetching data:', error));
-}, []);
 
 const movieData = [
   {
@@ -163,6 +156,15 @@ const movieData = [
 
 function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  useEffect(() => {
+    // Llamada a la API para obtener datos de películas
+    fetch('URL_DE_LA_API')
+      .then((response) => response.json())
+      .then((data) => setMovieData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+  
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
